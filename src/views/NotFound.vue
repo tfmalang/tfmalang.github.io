0f8910b82ab37eae089d7606466c988f3ccd57e8
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2024-09-14 15:08:44 +0700

    Add proper images to tfm

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,21 @@
 <script setup>
+import Button from '../components/Button.vue'
 </script>
 
 <template>
     <div class="min-h-screen w-full flex items-center justify-center">
         <div class="text-center">
-            <img src="../assets/404.png" alt="Page not found">
-            <!-- <router-link v-scroll-to="`#app`" to="/" class="text-blue-700">Home</router-link> -->
+            <i class="mdi mdi-alert-circle-outline text-9xl text-fdtm" />
+
+            <h1 class="text-4xl font-bold mb-2">Upps!</h1>
+            <p class="text-lg mb-4">Halaman yang anda tidak ditemukan</p>
+
+            <router-link to="/">
+                <Button>
+                    <i class="mdi mdi-home"></i>
+                    Kembali ke Beranda
+                </Button>
+            </router-link>
         </div>
     </div>
 </template>
